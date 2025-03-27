1f8fb5c3f142d28fe513ae7b7f6e2b764158f2cc
pascal <p.n.bakker@student.utwente.nl>
2025-03-27 16:52:29 +0100

    add message queues for playbook and log services

---
+++ services/playbookmanagement/src/main/java/nl/utwente/sosoc/playbookmanagement/AlarmsConsumer.java
@@ -0,0 +1,21 @@
+package nl.utwente.sosoc.playbookmanagement;
+
+import nl.utwente.sosoc.playbookmanagement.model.Alarm;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlarmsConsumer {
+
+
+    /**
+     * Receives an alarm from the {@code alarms} message queue and processes it.
+     *
+     * @param alarm the workflow instance
+     */
+    @JmsListener(destination = "alarms")
+    public void receiveMessage(Alarm alarm) {
+        System.out.println("Received alarm: " + alarm.getId() + " " + alarm.getRule().getName());
+    }
+
+}
